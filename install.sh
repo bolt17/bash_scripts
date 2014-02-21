beb41d27af6ffeb7ed47b09a0c59e08f7eff6e85
bolt17 <den.marych@gmail.com>
2014-02-21 12:27:29 +0200

    Create install.sh

---
+++ install.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+function check_root()
+{
+  if [ $EUID -ne 0 ]; then
+    echo "This script must be run as root" 1>&2
+    exit 1
+  fi
+}
+
+function configure_selinux()
+{
+  setenforce 0
+  sed -i s/'SELINUX=enforcing'/'SELINUX=disabled'/g /etc/selinux/config 
+}
+
+function stop_iptables()
+{
+  /etc/init.d/iptables stop
+}
+
+function install_salt()
+{
+  rpm -qa | grep epel-release-6-8
+  if [ $? -ne 0 ]; then
+    rpm -Uvh http://dl.fedoraproject.org/pub/epel/6/x86_64/epel-release-6-8.noarch.rpm
+    yum -y install salt-minion
+  fi
+}
